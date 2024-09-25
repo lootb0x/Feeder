89a6ff06450cac38bacc7df79937723b1f3b4702
lootb0x <justinlin8031@gmail.com>
2024-09-25 09:22:49 -0400

    Add files via upload

---
+++ Feeder.java
@@ -0,0 +1,44 @@
+
+  public class Feeder 
+  {
+   /**
+    * The amount of food, in grams, currently in the bird feeder; initialized
+    * in the constructor and always greater than or equal to zero
+    */
+   private int currentFood;
+
+   /**
+    * Simulates one day with numBirds birds or possibly a bear at the bird
+    * feeder, as described in part (a) Precondition: numBirds > 0
+    */
+
+   public void oneday (int numBirds)
+   {if(oneday<0)
+    system.out.println("less then 0 birds")
+   }
+   int Dailyfeed = (Math.random() * 41 + 9);   
+   int abnormality = (Math.random() * 20);
+   Dailyfeed *= numBirds 
+   currentFood -= dailyfeed
+   {if((curremtFood<0)
+     currentfood = 0
+     system.out.println("no more food in the feeder")
+   }
+   (if(abnormality = 1)
+   currentfood = 0
+   system.out.println("a bear got in no more food")
+   )
+
+
+   /**
+    * Returns the number of days birds or a bear found food to eat at the
+    * feeder in this simulation, as described in part (b) Preconditions:
+    * numBirds > 0, numDays > 0
+    */
+   public int many(int numBirds, int numDays) 
+   
+   {
+       return 0;
+   }
+
+}
---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main
+{    public static void main(String[] args)
+   ( 
+    Feeder f = new Feeder();
+    system.out.print(f)
+   )
