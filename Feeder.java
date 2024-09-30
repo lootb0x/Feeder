2dc4ec31588f742f5589a0a5b2eb9688300f0fe5
lootb0x <justinlin8031@gmail.com>
2024-09-30 13:19:39 +0000

    wababwabadoo

--- Feeder.java
+++ Feeder.java
@@ -1,16 +1,12 @@
 public class Feeder 
 {
 
-  /**
-   * The amount of food, in grams, currently in the bird feeder; initialized
-   * in the constructor and always greater than or equal to zero
-   */
   private int currentFood;
   public Feeder(int c)
   {
     currentFood = c;
   }
-  public int getcurrentFood()
+  public int getCurrentFood()
   {
     return currentFood;
   }
@@ -19,10 +15,6 @@ public class Feeder
       return "Current Food: " + currentFood;
   }
 
-  /**
-   * Simulates one day with numBirds birds or possibly a bear at the bird
-   * feeder, as described in part (a) Precondition: numBirds > 0
-   */
   public void simulateOneDay(int numBirds)
   {
     if(Math.random() < 0.05) currentFood = 0;
@@ -35,21 +27,18 @@ public class Feeder
 
   }
 
-  /**
-   * Returns the number of days birds or a bear found food to eat at the
-   * feeder in this simulation, as described in part (b) Preconditions:
-   * numBirds > 0, numDays > 0
-   */
   public int simulateManyDays(int numBirds, int numDays) 
   {
     int days = 0;
+    
      while(numDays > 0)
      {
+     if(currentFood == 0) return days;
          simulateOneDay(numBirds);
          numDays --;
          days ++; 
+         
      }
-     if(currentFood == 0) return days;
      return days;
   }
 
--- Main.java
+++ Main.java
@@ -1,6 +1,18 @@
 public class Main
 {    public static void main(String[] args)
    {
+      Feeder f = new Feeder(1);
+      System.out.println(f.getCurrentFood());
+      Feeder g = new Feeder(500);
+      System.out.println(g.getCurrentFood());
+      Feeder h = new Feeder(1000);
+      h.simulateOneDay(22);
+      System.out.println(h.getCurrentFood());
+      Feeder i = new Feeder(100);
+      i.simulateOneDay(5);
+      System.out.println(i);
+      Feeder j = new Feeder(2400);
+      System.out.println(j.simulateManyDays(10, 4));
       Feeder k = new Feeder(0);
       System.out.println(k.simulateManyDays(5, 10));
     
